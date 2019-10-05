b8228e5a2d9584eb992a6a34a26465b3a8b308e0
AJAY-MAC <letv4545@gmail.com>
2019-10-05 17:50:49 -0400

    Added Background and Player sprites

--- ChippyGame/GameScene.swift
+++ ChippyGame/GameScene.swift
@@ -10,11 +10,26 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-   
+    
+    var player:Player!
     
     override func didMove(to view: SKView) {
         // Set the background color of the app
         self.backgroundColor = SKColor.black;
+        let background = SKSpriteNode(imageNamed: "background")
+        background.size = self.size
+        background.position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+        background.zPosition = -1
+        addChild(background)
+        print("screen: \(self.size.width), \(self.size.height)")
+        
+        self.player = Player(imageNamed: "player")
+        self.player.size.width = self.size.width/13
+        self.player.size.height = self.size.height/13
+        self.player.position = CGPoint(x: self.size.width - 400, y: self.size.height / 2)
+        addChild(player)
+        
+        
     }
     
 }
