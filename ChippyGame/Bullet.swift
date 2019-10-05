bada3824775631f9fe7da62e433e2b160a5d9501
AJAY-MAC <letv4545@gmail.com>
2019-10-05 17:54:12 -0400

    Added Bullet model class

---
+++ ChippyGame/Bullet.swift
@@ -0,0 +1,31 @@
+//
+//  Bullets.swift
+//  ChippyGame
+//
+//  Created by AJAY BAJWA on 2019-10-05.
+//  Copyright © 2019 AJAY BAJWA. All rights reserved.
+//
+
+import SpriteKit
+
+// A custom SpriteNode class.
+// Used to represent a piece of sushi in the tower.
+class Bullet: SKSpriteNode {
+    
+    // MARK: Variables
+    // --------------------------------
+    // MARK: Constructor - required nonsense
+    // --------------------------------
+    override init(texture: SKTexture?, color: UIColor, size: CGSize) {
+        super.init(texture: texture, color: color, size: size)
+        
+    }
+    
+    // Required nonsense
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // Mark:  Functions
+    // --------------------------------
+}
